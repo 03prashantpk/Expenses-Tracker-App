8892ee2754a66c8a248e28f323d8cc9f742b9574
prashant kumar <prashantmanwan@gmail.com>
2023-02-17 14:16:39 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Expenses from "./Components/Expenses/Expenses";
+import NewExpanse from "./Components/NewExpense/NewExpanse";
+import Navbar from './Components/UI/navbar';
+import './Components/UI/home.css'
 
-function App() {
+const App = () => {
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    const storedExpenses = JSON.parse(localStorage.getItem('expenses'));
+    if (storedExpenses) {
+      const parsedExpenses = storedExpenses.map((expense) => {
+        return {
+          ...expense,
+          date: new Date(expense.date)
+        }
+      });
+      setExpenses(parsedExpenses);
+    }
+  }, []);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      const updatedExpenses = [expense, ...prevExpenses];
+      localStorage.setItem('expenses', JSON.stringify(updatedExpenses));
+      return updatedExpenses;
+    });
+  };
+
+  const years = [...new Set(expenses.map((expense) => expense.date.getFullYear()))];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="container">
+        <div className="column column1">
+          <NewExpanse onAddExpense={addExpenseHandler} />
+        </div>
+        <div className="column column2">
+          <div className="column2_outer">
+            <Expenses item={expenses} years={years} />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+// Written by github: @03prashantpk
